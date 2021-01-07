fc115ca768bec7cb112b9c6902b3d4a6e06cae4d
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-01-06 23:12:50 -0600

    go css.

--- script.js
+++ script.js
@@ -1,107 +1,105 @@
 // Javascript elements 
 var questions = [
-    {
-      question: 'what is 2+2',
-      answers: ['5', '6', '13', '4'],
-      correctAnswer: '4'
-    },
-    {
-      question: 'What color is the sky?',
-      answers: ['blue', 'gray', 'black', 'orange'],
-      correctAnswer: 'blue'
-    }
-    // questons more of them
-  ];
+  {
+    question: 'what is 2+2',
+    answers: ['5', '6', '13', '4'],
+    correctAnswer: '4'
+  },
+  {
+    question: 'What color is the sky?',
+    answers: ['blue', 'gray', 'black', 'orange'],
+    correctAnswer: 'blue'
+  },
+  {
+    question: 'what does HTML stand for?',
+    answers: ['HighText Machine Language ', 'HyperText and links Markup Language', 'HyperText Markup Language', 'HighText Machine Language?'],
+    correctAnswer: 'HyperText Markup Language'
+  },
+  {
+    question: 'How to create an unordered list (a list with the list items in bullets) in HTML?',
+    answers: ['ul', 'ol', 'li', 'div'],
+    correctAnswer: 'ul'
+  },
+  {
+    question: 'HTML tags are enclosed in?',
+    answers: ['! and ?', '< and >', '{ and }', '[and]'],
+    correctAnswer: '< and >'
+  }
+];
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var questionIndex = 0;
 var answers = document.querySelectorAll('.answer');
 var welcomeSec = document.querySelector('#welcomeSection');
 var quizSec = document.querySelector('#quizSection');
+var saveScore = document.querySelector('#saveScoreSection');
+var finalScore = document.querySelector('#finalScore');
 var timerEl = document.getElementById("time");
 var feedbackEl = document.getElementById("feedback");
-// add stop timer
-// var stopTimerEl = document.getElementById("stopTimer"); 
-
-
-
+var correctAnswers = 0;
 
 var startBtn = document.querySelector('#startButton');
+var timerId;
 
-
-//questions
 startBtn.addEventListener("click", startQuiz);
 
 
 function startQuiz() {
-  welcomeSec.setAttribute("class", "hidden");
-var timerId = setInterval(clockTick, 1000);
-// hides the Welcome section
+welcomeSec.setAttribute("class", "hidden");
+timerId = setInterval(clockTick, 1000);
 quizSec.setAttribute("class", "show");
 timerEl.textContent = time;
-// display/shows the Quiz section
+}
 
-}
 function clockTick(){
-    time --; 
-    timerEl.textContent = time;
-    if ( time <= 0 ) {
-      quizSec.classList.add("hidden");
-    }
+  time --; 
+  timerEl.textContent = time;
+  if ( time <= 0 ) {
+    timerEl.textContent = 0;
+    quizSec.classList.add("hidden");
+  }
 }
-// add stop function
-// function stopTimer() {
-//   time --;
-//   stopTimerEl
-//   console.log("stopTimer");
-// }
-// need to make loop and make a button set atrb
 
 function showQuestion() {
-  var questionEl = document.querySelector('.question');
-  var ans1 = document.querySelector('#answer1');
-  var currentQuestion = questions[currentQuestionIndex]
-//   var questionObj = questions[questionIndex];
-  questionEl.textContent = currentQuestion.question;
-  questions[questionIndex].answers.forEach(function(ans, i){
-    var btn = document.createElement("button");
-    btn.setAttribute("value", ans);
-    btn.textContent = ans;
-    btn.onclick = clickon;
-    ans1.append(btn);
-  })
-  //ans1.textContent = questionObj.answers[0];
-//   ans1.dataset('correct') = (3 === questionObj.correctAnswer)
-//   ans3.addEventListner('click', function() {
-//   })
-  questionIndex++;
+var questionEl = document.querySelector('.question');
+var ans1 = document.querySelector('#answer1');
+var currentQuestion = questions[currentQuestionIndex];
+questionEl.textContent = currentQuestion.question;
+ans1.textContent = "";
+currentQuestion.answers.forEach(function(ans, i){
+  var btn = document.createElement("button");
+  btn.setAttribute("value", ans);
+  btn.textContent = ans;
+  btn.onclick = clickon;
+  ans1.append(btn);
+})
 }
+
 function clickon(){
-    console.log(this.value)
-    if (this.value === questions[currentQuestionIndex].correctAnswer) {
-        console.log("awnser")
-        feedbackEl.textContent = "correct";
-    } else {
-        time -= 15;
-        timerEl.textContent = time;
-        feedbackEl.textContent = "wrong";
-    }
+  console.log(this.value)
+  if (this.value === questions[currentQuestionIndex].correctAnswer) {
+    correctAnswers++;
+    console.log("awnser")
+    feedbackEl.textContent = "correct";
+  } else {
+    time -= 15;
+    timerEl.textContent = time;
+    feedbackEl.textContent = "wrong";
+  }
+currentQuestionIndex++;
+if(currentQuestionIndex === questions.length) {
+  quizSec.setAttribute("class", "hidden");
+  saveScore.setAttribute("class", "show");
+  finalScore.textContent = correctAnswers;
+  clearInterval(timerId);
+  timerEl.textContent = 0;
+  feedbackEl.textContent = "";
+} else {
+  showQuestion();
 }
-showQuestion()
+}
 
-
-// clearInterval(timerId);  Stops the process
-
-
-
-
-
-
-
-
-
-
-
+showQuestion();
 
 // // questions array
 //  questions = [
@@ -110,19 +108,19 @@ showQuestion()
 //         question : "what does HTML stand for?" ,
 
 //         choiceA : "HighText Machine Language",
-    
+  
 //         choiceB : "HyperText and links Markup Language",
 
 //         choiceC : "HyperText Markup Language",
 
 //         correct : "HyperText Markup Language"
-    
+  
 //     },{
 
 //         question : "How to create an unordered list (a list with the list items in bullets) in HTML?" ,
 
 //         choiceA : "<ul>",
-    
+  
 //         choiceB : "<ol>" ,
 
 //         choiceC : "<li>" ,
@@ -134,7 +132,7 @@ showQuestion()
 //         question : " HTML tags are enclosed in-" ,
 
 //         choiceA : "! and ?",
-    
+  
 //         choiceB : "< and >" ,
 
 //         choiceC : "{ and }" ,
@@ -146,7 +144,7 @@ showQuestion()
 //         question : "How to create a hyperlink in HTML?" ,
 
 //         choiceA : "<a href = www.javatpoint.com> javaTpoint.com </a>",
-    
+  
 //         choiceB : "<a url = www.javatpoint.com javaTpoint.com /a>" ,
 
 //         choiceC : "<a link = www.javatpoint.com> javaTpoint.com </a>" ,
